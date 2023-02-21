4ad98a22681df37646d6f5c8ecfb1b9323eae59b
Solirix <hades5132@gmail.com>
2023-02-20 22:25:55 -0500

    Created portfolio render request

--- website/core/views.py
+++ website/core/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'core/index.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
