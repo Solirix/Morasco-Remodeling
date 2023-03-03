c14f21264909168d20fb8410b1d4e781b3411641
rome6315 <romo6315@gmail.com>
2023-03-03 00:28:42 -0500

    UI tests for nav bar

--- website/core/tests/test_navigation_UI.py
+++ website/core/tests/test_navigation_UI.py
@@ -1,5 +1,6 @@
 # pip install selenium, webdriver-manager
 
+import time
 from django.test import LiveServerTestCase # needed for testing the UI
 from selenium import webdriver 
 from webdriver_manager.chrome import ChromeDriverManager # this line and the next can be changed to use other browsers
@@ -20,9 +21,34 @@ class TestUI(LiveServerTestCase):
 
         assert "Portfolio" in self.driver.title # check if the title of the page is "Portfolio"
 
+    # make sure the logo is clickable and takes you to the home page
     def test_image_link(self):
         self.driver.get('http://127.0.0.1:8000/portfolio')
         logo = self.driver.find_element(By.ID, "logo")
+        time.sleep(2)
         logo.click()
         assert "Home" in self.driver.title
+
+    # make sure the navbar links are clickable and take you to the correct page
+    def test_navbar_links(self):
+        self.driver.get('http://127.0.0.1:8000/')
+        link = self.driver.find_element(By.ID, "service nav")
+        link.click()
+        time.sleep(2)
+        assert "Services" in self.driver.title
+
+        link = self.driver.find_element(By.ID, "portfolio nav")
+        link.click()
+        time.sleep(2)
+        assert "Portfolio" in self.driver.title
+
+        link = self.driver.find_element(By.ID, "about nav")
+        link.click()
+        time.sleep(2)
+        assert "About" in self.driver.title
+
+        link = self.driver.find_element(By.ID, "contact nav")
+        link.click()
+        time.sleep(2)
+        assert "Contact" in self.driver.title
 
