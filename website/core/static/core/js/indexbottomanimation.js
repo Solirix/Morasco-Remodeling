68b14d5b934425209153c6c1519467b4e83b25ba
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2023-04-05 04:08:25 -0400

    animations bottom index

--- website/core/static/core/js/indexbottomanimation.js
+++ website/core/static/core/js/indexbottomanimation.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     var words = ["BUILT", "DESIGNED", "DEVELOPED", "CREATED", "ENGINEERED", "MADE", "CONSTRUCTED", "CONCOCTED", "CONSTRUCTED"];
     var rotatingWord = document.getElementById("rotating-word");
+    rotatingWord.classList.add("rotating-word"); // Add the class to the element
     var index = 0;
     setInterval(function() {
-    rotatingWord.innerHTML = words[index];
-    index++;
-    if (index == words.length) {
-        index = 0;
-    }
-    }, 2000);
-
+      rotatingWord.innerHTML = words[index];
+      index++;
+      if (index == words.length) {
+          index = 0;
+      }
+    }, 3500);
   });
+  
