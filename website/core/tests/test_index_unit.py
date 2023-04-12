f07a180bf46dab090a30bb0773c13b5b3d822cb0
Solirix <hades5132@gmail.com>
2023-04-12 14:53:30 -0400

    unit test written for index about us section text

--- website/core/tests/test_index_unit.py
+++ website/core/tests/test_index_unit.py
@@ -6,3 +6,7 @@ class portfolio_page_tests(TestCase):
     def test_images_loading(self):
         response = self.client.get('/index/')
         self.assertContains(response, 'core/images/Squared/about-us-pic(1)"')
+
+    def test_about_us_section(self):
+        response = self.client.get('/index/')
+        self.assertContains(response, 'class="slanted-text-container"')
