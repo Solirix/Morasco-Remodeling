608c80c779007ba9d150a3cc608aaacc54018024
Solirix <hades5132@gmail.com>
2023-04-24 19:20:22 -0400

    missing contact us unit tests written

--- website/core/tests/test_contact_unit.py
+++ website/core/tests/test_contact_unit.py
@@ -15,7 +15,15 @@ class contact_us_page_tests(TestCase):
     def test_contact_us_input(self):
         response = self.client.get('/contact/')
         self.assertContains(response, 'class="input-group"')
+        self.assertContains(response, 'id="email"')
+        self.assertContains(response, 'id="message"')
+        self.assertContains(response, 'id="number"')
+        self.assertContains(response, 'id="name"')
 
     def test_contact_us_button(self):
         response = self.client.get('/contact/')
-        self.assertContains(response, 'class="button"')
+        self.assertContains(response, 'class="button"') 
+
+    def test_contact_us_rows(self):
+        response = self.client.get('/contact/')
+        self.assertContains(response, 'class="row"')
