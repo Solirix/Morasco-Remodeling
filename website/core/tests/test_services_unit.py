0c090194067983bf8cf935e83344108b832ebd88
rome6315 <romo6315@gmail.com>
2023-04-25 10:50:43 -0400

    update services unit tests

--- website/core/tests/test_services_unit.py
+++ website/core/tests/test_services_unit.py
@@ -7,17 +7,17 @@ class services_page_tests(TestCase):
 
     def test_css_being_called(self):
         response = self.client.get('/services/')
-        self.assertContains(response, 'class="services_page_container"')
+        self.assertContains(response, 'class="services_row_container"')
         self.assertContains(response, 'id="services_page_header"')
         self.assertContains(response, 'class="service_container"')
         self.assertContains(response, 'class="service_heading"')
-        self.assertContains(response, 'class="service_text"')
+        self.assertContains(response, 'class="services_list"')
         self.assertContains(response, 'class="didnt_see_container"')
         self.assertContains(response, 'id="didnt_see_text"')
         self.assertContains(response, 'id="ask_us_today"')
 
     def test_images_loading(self):
         response = self.client.get('/services/')
-        self.assertContains(response, 'core/images/normal_sized/deck-with-seats.jpg')
+        self.assertContains(response, 'core/images/normal_sized/heated_floor.jpg')
         self.assertContains(response, 'core/images/normal_sized/wood-wall-basement.jpg')
 
