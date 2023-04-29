9f2414f653b302e76a58abb1a5dd3c32848d0109
rome6315 <romo6315@gmail.com>
2023-04-29 14:18:51 -0400

    remove staticfiles directory

--- website/staticfiles/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig # configure the app
-
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
--- website/staticfiles/core/js/services.js
+++
@@ -1,8 +0,0 @@
-//?How Many Paths?
-window.addEventListener('load', function() {
-  //On load the animation will start on .services_images class
-    var elements = document.querySelectorAll('.services_images');
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].style.opacity = 1;
-    }
-  });
