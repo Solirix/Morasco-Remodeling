7b8a3ceb4967a875dcacb170209459d09ede7781
rome6315 <romo6315@gmail.com>
2023-04-29 17:40:04 -0400

    delete files

--- website/staticfiles/core/js/portfolio.js
+++
@@ -1,8 +0,0 @@
-//?How many Paths?
-addEventListener('load', function() {
-        //On load the animation will start on .port_photos class
-        var elements = document.querySelectorAll('.port_photos');
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].style.opacity = 1;
-        }
-      });
